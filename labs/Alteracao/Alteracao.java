0e4cb8e22303e29f7d6b9d8942ebf4a5489c2d9d
rafaelm <rafa_maluf@yahoo.com.br>
2023-08-09 17:15:32 -0300

    update

---
+++ labs/Alteracao/Alteracao.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+    class Alteracao{
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String palavra = sc.nextLine();
+            Random gerador = new Random( );
+                   gerador.setSeed (4);
+            while (!palavra.equals("FIM")){
+                char x = ((char) ('a' + (Math.abs (gerador.nextInt( )) % 26)));
+                char y = ((char) ('a' + (Math.abs (gerador.nextInt( )) % 26)));
+              
+              
+              palavra = palavra.replace(x,y);
+              
+              System.out.println(palavra);
+              palavra = sc.nextLine();
+                
+            }
+        }
+        
+        
+    }
